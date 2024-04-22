79c0a5b74ce797be34bdf3d6bfd09145a5337bbe
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-04-22 23:14:08 +0300

    add AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./utils/auth";
+import { Navigate } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+      <AuthProvider navigate={Navigate}>
+        <App />
+      </AuthProvider>
+  </React.StrictMode>
+);
