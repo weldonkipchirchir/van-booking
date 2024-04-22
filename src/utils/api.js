ffd232a5ff2c69994e75135241d676ef7d948826
weldon kipchirchir <weldonkipchirchir23@gmail.com>
2024-04-22 23:13:21 +0300

    add vans-list/:id endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,68 @@
 export async function getVans() {
-  const res = await fetch("/api/vans");
-  if (!res.ok) {
-    throw {
-        message: "Failed to fetch vans", 
-        statusText: res.statusText,
-        status: res.status
+    const res = await fetch("/api/vans");
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
     }
+    const data = await res.json();
+    return data.vans;
 }
-  const data = await res.json();
-  return data.vans;
+
+export async function getHostVans() {
+    const res = await fetch("/api/host/vans");
+    if (!res.ok) {
+        throw {
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json();
+    return data.vans;
+}
+
+export async function getHostVanId(vanId) {
+    const res = await fetch(`/api/host/vans/${vanId}`);
+    if (!res.ok) {
+        throw { 
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json();
+    return data.vans;
+}
+
+export async function getVanListVanId(id) {
+    const res = await fetch(`/api/vans/vans-list/${id}`);
+    if (!res.ok) {
+        throw { 
+            message: "Failed to fetch vans",
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+    const data = await res.json();
+    return data.vans;
+}
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/sign-in",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
 }
